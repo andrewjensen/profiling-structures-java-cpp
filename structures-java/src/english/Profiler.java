9787e925be08881e100c4cce0d722c91e53f907b
Joey Cozza <joeycozza@gmail.com>
2013-03-20 10:47:45 -0700

    Working on Profiler

--- structures-java/src/english/Profiler.java
+++ structures-java/src/english/Profiler.java
@@ -1,6 +1,5 @@
 package english;
-import java.util.ArrayList;
-import java.util.Collections;
+import java.util.*;
 public class Profiler {
 
 	public static void main(String[] args) {
@@ -10,23 +9,14 @@ public class Profiler {
 		
 		for(int k=0; k<TEST_RUNS; k++){
 		
-			ArrayList<Double> arr = new ArrayList<Double>();
+			List<Double> arr = new ArrayList<Double>();
 			
 			
-			long startTime, collectionFillTime, presortedTime, totalTime, sortedTime;
-			
-			/*
-			 * This gets the current time, fills the collection with random numbers
-			 * and gets the time afterwards.  
-			 * randArrayFill is the time it takes to fill the collection.
-			 */
-			startTime = System.nanoTime();
-			for(int i =0; i<3000000; i++){
-				arr.add(Math.random());
-			}
-			collectionFillTime = System.nanoTime() - startTime;
+			long fillTime, collectionFillTime, presortedTime, totalTime, sortTime;
 			
 			
+			fillTime = fillCollection(arr);
+			sortTime = sortCollection(arr);
 			
 			//WE SHOULD WRITE A TEST FOR FINDING HERE.
 			//IT SHOULD CHOOSE LIKE 1000 OBJECTS THAT ARE FOR SURE IN THE COLLECTION
@@ -35,22 +25,62 @@ public class Profiler {
 			
 			
 			
-			/*
-			 * This gets the time for sorting the collection.  
-			 */
-			presortedTime = System.nanoTime();
-			Collections.sort(arr);
-			sortedTime = System.nanoTime() - presortedTime;
+			
+			
 			
 			/*
 			 * Just outputs the time taken for each iteration of the test.
 			 */
-			totalTime = System.nanoTime()-startTime;
-			System.out.println("Fill Time: " + collectionFillTime + ". Sort Time: "+ sortedTime + ". TotalTime: "+totalTime+ ".");
+			totalTime = fillTime + sortTime;
+			System.out.println("Fill Time: " + fillTime + ". Sort Time: "+ sortTime + ". TotalTime: "+totalTime+ ".");
 			masterTime +=totalTime;
 		}
 		
 		averageTime = masterTime/TEST_RUNS;
 		System.out.println("average Total Time: "+averageTime);
 	}
+	
+	
+	/*
+	 * This gets the current time, fills the collection with random numbers
+	 * and gets the time afterwards.  
+	 * randArrayFill is the time it takes to fill the collection.
+	 */
+	static long fillCollection(Collection col){
+		
+		long startTime, fillTime;
+		
+		startTime = System.nanoTime();
+		for(int i =0; i<3000000; i++){
+			col.add(Math.random());
+		}
+		fillTime = System.nanoTime() - startTime;
+		
+		return fillTime;
+	}
+	
+	
+	/*
+	 * This gets the time for sorting the collection.
+	 * Returns -1 if we aren't actually sorting the collection.  
+	 */
+	static long sortCollection(Collection col){
+		long startTime, sortTime;
+		
+		startTime = System.nanoTime();
+		
+		//THIS ONLY WORKS ON LIST<T> ...SO WHAT DO WE WANT TO DO FOR THIS TESTING?
+		//WE COULD ONLY USE IT ON LIST OBJECTS, THAT WOULD BE FINE.
+		//Collections.sort(col);
+		
+		sortTime = System.nanoTime() - startTime;
+		if(sortTime < 2000)
+			sortTime = -1;
+		
+		return sortTime;
+	}
+	
+	
+	
+	
 }
