53c7c64188b2ce9296a2a3ec142e6507cb739294
Joey Cozza <joeycozza@gmail.com>
2013-03-24 13:01:13 -0700

    I did LinkedList_Impl, but NOT templated.  It is built for strings right now. Didn't have time for templating. The little tests makes it look like it is working correctly.

--- structures-cpp/linkedlist_impl.h
+++ structures-cpp/linkedlist_impl.h
@@ -1,27 +1,114 @@
 #ifndef linkedlist_impl_h
 #define linkedlist_impl_h
 
-#include <iostream>
 #include <string>
-
 using namespace std;
 
-class LinkedList_Impl{
-	public:
+//! LLNode implements a linked list node
+class LLNode {
 
-		LinkedList_Impl(){
+	friend class LinkedList_Impl; // LinkedList can access private members of LLNode
+public:
 
+	//!  Constructor
+	LLNode(const int v, LLNode * n) :
+			value(v), next(n) {
+	}
+
+	//! Copy Constructor
+	LLNode(const LLNode& other) :
+			value(other.value), next(other.next) {
+	}
+
+	//! Assignment operator
+	LLNode& operator=(const LLNode& other) {
+		if (this != &other) {
+			value = other.value;
+			next = other.next;
 		}
+		return *this;
+	}
 
-		~LinkedList_Impl(){
+	int getValue() {
+		return value;
+	}
 
+	LLNode* getNext() {
+		return next;
+	}
+
+private:
+	int value; //!< value stored in the node
+	LLNode* next; //!< pointer to next node in the list
+};
+
+//! LinkedList implements a doubly-linked list
+class LinkedList_Impl {
+public:
+
+	//!  No-arg constructor.  Initializes an empty linked list
+	LinkedList_Impl() {
+		size = 0;
+		root = NULL;
+	}
+
+	//!  Destructor
+	~LinkedList_Impl();
+
+	//!  @return the number of values in the list
+	int getSize() const {
+		return size;
+	}
+
+	//!  Inserts value v into the list after node n
+	//!
+	//!  @param v The new value being inserted
+	//!  @param n A node that is already in the list after which the new node should
+	//!      be inserted.
+	//!      If n is NULL, the new node should be inserted at the beginning of the list.
+	//!
+	//!  @return a pointer to the newly inserted node
+	bool add(const int v) {
+		if (root == NULL) {
+			root = new LLNode(v, NULL);
+			size++;
+			return true;
+		} else {
+			LLNode* newNode = new LLNode(v, NULL);
+			LLNode* curNode = root;
+			while (curNode->next != NULL) {
+				curNode = curNode->next;
+			}
+			curNode->next = newNode;
+			size++;
+			return true;
 		}
+	}
 
+	//! Searches for the first occurrence of value v that appears in the list
+	//!   after node n
+	//!
+	//!  @param v The value being searched for
+	//!  @return true if found, false otherwise
+	bool contains(const int v) const {
+		LLNode* curNode = root;
 
+		while (curNode != NULL) {
+			if (curNode->value == v) {
+				return true;
+			} else
+				curNode = curNode->next;
+		}
+		return false;
+	}
 
-	private:
+	LLNode* getRoot() {
+		return root;
+	}
 
-
+private:
+	int size;
+	LLNode* root;
 };
 
 #endif
