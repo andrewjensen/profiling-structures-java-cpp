469f52839e3cef082ba31435f16a4bab665e774f
Andrew Jensen <andrewjensen90@gmail.com>
2013-03-25 18:25:30 -0600

    Joey, you look at this!!

--- structures-cpp/arraylist_impl.h
+++ structures-cpp/arraylist_impl.h
@@ -6,20 +6,70 @@
 
 using namespace std;
 
-class ArrayList_Impl{
+class ArrayList_Impl
+{
 	public:
+		static const int INITIAL_ARRAY_LENGTH = 10;
+		static const int GROW_FACTOR = 2;
 
-		ArrayList_Impl(){
+		ArrayList_Impl()
+		{
+			size = 0;
+			arrayLength = INITIAL_ARRAY_LENGTH;
+			//elements = string[INITIAL_ARRAY_LENGTH];
+		}
+
+		~ArrayList_Impl()
+		{
 
 		}
 
-		~ArrayList_Impl(){
+		bool add(string item)
+		{
+			if (size == arrayLength)
+				resizeArray();
 
+			elements[size] = item;
+			size++;
+
+			return true;
 		}
 
+		bool contains(string item)
+		{
+			for (int i = 0; i < size; i++)
+				if (elements[i] == item)
+					return true;
 
+			return false;
+		}
+
+		string get(int index)
+		{
+			return elements[index];
+		}
+
+		int getSize() { return size; }
+
+		bool isEmpty() { return size == 0; }
+
+		void resizeArray()
+		{
+			int newArrayLength = arrayLength * GROW_FACTOR;
+
+			string newElements[newArrayLength];
+
+			for (int i = 0; i < arrayLength; i++)
+				newElements[i] = elements[i];
+
+			arrayLength = newArrayLength;
+			elements = newElements;	//FIXME: is this valid?
+		}
 
 	private:
+		string elements[INITIAL_ARRAY_LENGTH];
+		int size;
+		int arrayLength;
 
 
 };
