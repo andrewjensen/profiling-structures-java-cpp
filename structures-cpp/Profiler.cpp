e6dda0c19bf505b45c0e374d6ab5041cdc2a5e63
Joey Cozza <joeycozza@gmail.com>
2013-03-27 17:52:10 -0700

    std::set

--- structures-cpp/Profiler.cpp
+++ structures-cpp/Profiler.cpp
@@ -21,13 +21,13 @@ const int TEST_RUNS = 10;
 
 using namespace std;
 
-long fillCollection(Set_Impl* col) {
+long fillCollection(set<double>* col) {
 
 	clock_t start = clock();
 
 	for (int i = 0; i < 600000; i++) {
 
-		col->add((double) rand());
+		col->insert((double) rand());
 
 	}
 
@@ -36,7 +36,7 @@ long fillCollection(Set_Impl* col) {
 	return (((fill - start) * 1000000000) / CLOCKS_PER_SEC);
 }
 
-long findCollection(Set_Impl* col) {
+long findCollection(set<double>* col) {
 
 	clock_t start = clock();
 
@@ -48,7 +48,7 @@ long findCollection(Set_Impl* col) {
 //			if (*it == (double) i)
 //				break;
 //		}
-		col->contains((double) i);
+		col->find((double) i);
 	}
 	clock_t find = clock();
 
@@ -57,16 +57,15 @@ long findCollection(Set_Impl* col) {
 
 int main(int argc, const char* argv[]) {
 
-	//set<double>* col = new set<double>();
+	set<double>* col = new set<double>();
 	//list<double>* col = new list<double(); // HOW TO MAKE THIS A LINKEDLIST?
-	Set_Impl* col = new Set_Impl();
 
 	long fillTimes[TEST_RUNS], findTimes[TEST_RUNS];
 	cout << "Starting" << endl;
 	for (int i = 0; i < TEST_RUNS; i++) {
 
 		cout << i + 1 << " " << endl;
-		col = new Set_Impl();
+		col = new set<double>();
 
 		fillTimes[i] = fillCollection(col);
 		findTimes[i] = findCollection(col);
